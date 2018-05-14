09b538a7e099213c821ac7e3625e5f20403a39a2
keyman9 <laxscott@gmail.com>
2018-05-14 16:16:34 -0600

    Built with index page

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -36,5 +36,5 @@ class FormContainer extends Component {
 }
 
 const wrapper = document.getElementById("create-article-form");
-wrapper ? ReactDOM:render(<FormContainer />, wrapper) : false;
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
 export default FormContainer;
